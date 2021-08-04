35a96963be58d0b3e27e16c40f86330c7657a0ee
retrocode <630413133@qq.com>
2021-08-04 21:07:49 +0800

    1.兼容php7.4

--- public/checkboard.php
+++ public/checkboard.php
@@ -34,8 +34,8 @@ if (!empty($querycode)) {
 </head>
 
 <body style='padding: 10px;'>
-    <div style='border-bottom: 1px solid #DCDFE6;padding-bottom: 10px;'>阅读数: <? echo $result[0]['GET_COUNT'] ?></div>
-    <textarea id='ipt' readonly style='margin: 10px 0;border: 1px solid #DCDFE6;padding: 10px;display: block;width: 100%;box-sizing: border-box;outline: none;resize: none;color:rgb(75, 85, 99);'><? echo $result[0]['MESSAGE'] ?> </textarea>
+    <div style='border-bottom: 1px solid #DCDFE6;padding-bottom: 10px;'>阅读数: <?php echo $result[0]['GET_COUNT'] ?></div>
+    <textarea id='ipt' readonly style='margin: 10px 0;border: 1px solid #DCDFE6;padding: 10px;display: block;width: 100%;box-sizing: border-box;outline: none;resize: none;color:rgb(75, 85, 99);'><?php echo $result[0]['MESSAGE'] ?> </textarea>
     <button id='btn' style='border: 1px solid rgb(229, 231, 235);background-color: transparent;padding: 5px 20px;box-shadow: 0 4px 6px -1px rgb(0 0 0 / 10%), 0 2px 4px -1px rgb(0 0 0 / 6%);border-radius: 6px;color: rgb(107, 114, 128);'>复制</button>
 </body>
 <script type="text/javascript">
